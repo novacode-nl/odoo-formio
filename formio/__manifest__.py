ad667f01f2da7d161cb06144d5bd97cc3b98368a
Bob Leers <bob@novacode.nl>
2018-10-01 17:35:51 +0200

    formio: Remove website dependency and template extending.

--- formio/__manifest__.py
+++ formio/__manifest__.py
@@ -10,7 +10,7 @@
     'website': 'https://www.novacode.nl',
     'license': 'LGPL-3',
     'category': 'Extra Tools',
-    'depends': ['website', 'base'],
+    'depends': ['base'],
     'data': [
         'views/formio_builder_views.xml',
         'views/formio_menu.xml',
