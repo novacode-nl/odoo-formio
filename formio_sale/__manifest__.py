a7cee9f2df95039bcaf4729c82d30933dcfe57be
Bob Leers <bob@novacode.nl>
2018-10-04 00:11:48 +0200

    formio_sale: Some module info changes.

--- formio_sale/__manifest__.py
+++ formio_sale/__manifest__.py
@@ -4,24 +4,24 @@
 
 {
     'name': 'Form.io - Sales',
-    'summary': 'Form.io webforms on Sale Orders',
+    'summary': 'Form.io webforms on Sale Orders/Quotes',
     'version': '0.1',
     'author': 'Nova Code',
     'website': 'https://www.novacode.nl',
     'license': 'LGPL-3',
-    'category': 'Extra Tools',
+    'category': 'Sales',
     'depends': ['sale', 'formio'],
     'data': [
         'views/sale_views.xml',
-        # 'views/formio_form_views.xml',
-        # 'views/formio_menu.xml',
     ],
     'application': True,
     'images': [
         'static/description/banner.png',
     ],
     'description': """
-Form.io Sales
-=============
+Form.io - Sales
+===============
+
+Build and publish (Form.io) forms for Sale Orders/Quotes.
 """
 }
