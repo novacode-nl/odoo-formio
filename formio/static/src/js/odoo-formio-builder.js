5ea253012059a4f45d5d064b5965ece7fd7f01bd
Bob Leers <bob@novacode.nl>
2018-10-03 21:22:22 +0200

    formio: Builder save occurs on change event. Not by save/delete Component.

--- formio/static/src/js/odoo-formio-builder.js
+++ formio/static/src/js/odoo-formio-builder.js
@@ -19,30 +19,13 @@ odoo.define('formio.Builder', ['web.ajax'], function (require) {
 
             // Render the Builder (with JSON schema).
             Formio.builder(document.getElementById('formio_builder'), schema).then(function(builder) {
-                builder.on('saveComponent', function(component) {
+                builder.on('change', function(res) {
+                    console.log('[Form.io] Saving Builder...');
                     ajax.jsonRpc(save_url, 'call', {
                         'builder_id': builder_id,
                         'schema': builder.schema
-                    }).then(function (result) {
-                        console.log('Form.io: saved component '+ component.key);
-                    });
-                });
-                builder.on('deleteComponent', function(component) {
-                    // TODO Update the builder.schema - Prevent data loss!
-                    // Instead building a new_schema['components'] ...
-                    // change the present builder.schema.components (by slice()).
-                    var new_schema = {'components': []};
-                    for (var i=0; i < builder.schema.components.length; i++) {
-                        // Check component deleted by key.
-                        if (builder.schema.components[i].key != component.key) {
-                            new_schema['components'].push(builder.schema.components[i]);
-                        }
-                    }
-                    ajax.jsonRpc(save_url, 'call', {
-                        'builder_id': builder_id,
-                        'schema': new_schema
-                    }).then(function (result) {
-                        console.log('Form.io: deleted component '+ component.key);
+                    }).then(function() {
+                        console.log('[Form.io] Builder sucessfully saved.');
                     });
                 });
             });
