98c9ca6f581a2c326100d0f92b2b30acd99a2d1e
Bob Leers <bob@novacode.nl>
2018-10-12 12:45:27 +0200

    formio_sale: Forms button as action. Set context (search-view and defaults).

--- formio_sale/models/sale.py
+++ formio_sale/models/sale.py
@@ -10,10 +10,16 @@ class SaleOrder(models.Model):
 
     formio_forms = fields.One2many('formio.form', 'sale_order_id', string='Form.io Forms')
     formio_forms_count = fields.Integer(compute='_compute_formio_forms_count')
+    formio_this_model_id = fields.Many2one('ir.model', compute='_compute_formio_this_model_id')
 
     def _compute_formio_forms_count(self):
         for r in self:
-            self.formio_forms_count = len(r.formio_forms)
+            r.formio_forms_count = len(r.formio_forms)
+
+    def _compute_formio_this_model_id(self):
+        self.ensure_one()
+        model_id = self.env.ref('sale.model_sale_order').id
+        self.formio_this_model_id = model_id
 
     @api.multi
     def action_formio_forms(self):
@@ -25,7 +31,7 @@ class SaleOrder(models.Model):
             'domain': [('res_id', '=', self.id), ('res_model_id', '=', res_model_id)],
             'context': {'default_res_id': self.id},
             'view_type': 'form',
-            'view_mode': 'tree,form',
+            'view_mode': 'kanban,tree,form',
             'res_model': 'formio.form',
             'view_id': False,
         }
