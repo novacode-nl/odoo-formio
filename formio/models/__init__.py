96610c83a570050df7baf0941f37b7840fddce1a
Bob Leers <bob@novacode.nl>
2019-02-05 13:07:18 +0100

    formio: rename (model)file to formio_translation.

--- formio/models/__init__.py
+++ formio/models/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 Nova Code (http://www.novacode.nl)
+# Copyright Nova Code (http://www.novacode.nl)
 # See LICENSE file for full licensing details. -->
 
 from . import formio_builder
 from . import formio_form
 from . import formio_version
 from . import formio_version_asset
-from . import formio_version_translation
+from . import formio_translation
