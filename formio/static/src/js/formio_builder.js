90c8ab6c7469b2884b88ec36a0347828f33a7b7f
Bob Leers <bob@novacode.nl>
2020-02-24 10:57:51 +0100

    [UPD] formio: Drop Odoo Javascript loading. Add standalone JsonRPC client.

--- formio/static/src/js/formio_builder.js
+++ formio/static/src/js/formio_builder.js
@@ -1,43 +1,37 @@
 // Copyright Nova Code (http://www.novacode.nl)
 // See LICENSE file for full licensing details.
 
-odoo.define('formio.formio_builder_embed',  ['web.ajax'], function (require) {
-    "use strict";
+$(document).ready(function() {
+    var builder_id = document.getElementById('builder_id').value,
+        schema_url = '/formio/builder/schema/' + builder_id,
+        options_url = '/formio/builder/options/' + builder_id,
+        save_url = '/formio/builder/save/' + builder_id,
+        schema = {};
 
-    var ajax = require('web.ajax');
+    $.jsonRpc.request(options_url, 'call', {}).then(function(_options) {
+        var options = JSON.parse(_options);
 
-    $(document).ready(function() {
-        var builder_id = document.getElementById('builder_id').value,
-            schema_url = '/formio/builder/schema/' + builder_id,
-            options_url = '/formio/builder/options/' + builder_id,
-            save_url = '/formio/builder/save/' + builder_id,
-            schema = {};
+        $.jsonRpc.request(schema_url, 'call', {}).then(function(result) {
+            if (!$.isEmptyObject(result)) {
+                schema = JSON.parse(result); // Form JSON schema
+            }
 
-        ajax.jsonRpc(options_url, 'call', {}).then(function(_options) {
-            var options = JSON.parse(_options);
-
-            ajax.jsonRpc(schema_url, 'call', {}).then(function(result) {
-                if (!$.isEmptyObject(result)) {
-                    schema = JSON.parse(result); // Form JSON schema
-                }
-    
-                // Render the Builder (with JSON schema).
-                Formio.builder(document.getElementById('formio_builder'), schema).then(function(builder) {
-                    builder.on('change', function(res) {
-                        if ('readOnly' in options && options['readOnly'] == true) {
-                            alert("This Form Builder is readonly. It's state is either Current or Obsolete. Refresh the page again.");
-                            return;
-                        }
-                        else {
-                            console.log('[Form.io] Saving Builder...');
-                            ajax.jsonRpc(save_url, 'call', {
-                                'builder_id': builder_id,
-                                'schema': builder.schema
-                            }).then(function() {
-                                console.log('[Form.io] Builder sucessfully saved.');
-                            });
-                        }
-                    });
+            // Render the Builder (with JSON schema).
+            Formio.builder(document.getElementById('formio_builder'), schema).then(function(builder) {
+                builder.on('change', function(res) {
+                    if ('readOnly' in options && options['readOnly'] == true) {
+                        alert("This Form Builder is readonly. It's state is either Current or Obsolete. Refresh the page again.");
+                        return;
+                    }
+                    else {
+                        console.log('[Form.io] Saving Builder...');
+                        $.jsonRpc.request(save_url, 'call', {
+                            'builder_id': builder_id,
+                            'schema': builder.schema
+                        }).then(function() {
+                            console.log('[Form.io] Builder sucessfully saved.');
+                        });
+                    }
                 });
             });
         });
