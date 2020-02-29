92b2e005ee75cad2d52c2b46fd45bee26867cb4b
Bob Leers <bob@novacode.nl>
2020-02-29 09:38:52 +0100

    [UPD] formio form: Drop Odoo Javascript loading. Use standalone JsonRPC client.

--- formio/static/src/js/formio_form.js
+++ formio/static/src/js/formio_form.js
@@ -1,71 +1,63 @@
 // Copyright Nova Code (http://www.novacode.nl)
 // See LICENSE file for full licensing details.
 
-odoo.define('formio.formio_form_embed', ['web.ajax'], function (require) {
-    "use strict";
+$(document).ready(function() {
+    var uuid = document.getElementById('form_uuid').value,
+        base_url = window.location.protocol + '//' + window.location.host,
+        schema_url = '/formio/form/schema/' + uuid,
+        options_url = '/formio/form/options/' + uuid,
+        submission_url = '/formio/form/submission/' + uuid,
+        submit_url = '/formio/form/submit/' + uuid,
+        schema = {},
+        options = {};
 
-    // TODO Get rid of callbacks and refactor in a classy way.
-    // This should init the Form with schema_url and options_url.
-    var ajax = require('web.ajax');
+    $.jsonRpc.request(schema_url, 'call', {}).then(function(result) {
+        if (!$.isEmptyObject(result)) {
+            schema = JSON.parse(result);
 
-    $(document).ready(function() {
-        var uuid = document.getElementById('form_uuid').value,
-            base_url = window.location.protocol + '//' + window.location.host,
-            schema_url = '/formio/form/schema/' + uuid,
-            options_url = '/formio/form/options/' + uuid,
-            submission_url = '/formio/form/submission/' + uuid,
-            submit_url = '/formio/form/submit/' + uuid,
-            schema = {},
-            options = {};
-
-        ajax.jsonRpc(schema_url, 'call', {}).then(function(result) {
-            if (!$.isEmptyObject(result)) {
-                schema = JSON.parse(result);
-
-                ajax.jsonRpc(options_url, 'call', {}).then(function(_options) {
-                    var options = JSON.parse(_options);
-                    var hooks = {
-                        'addComponent': function(container, comp, parent) {
-                            if (comp.hasOwnProperty('component') && comp.component.hasOwnProperty('data') &&
-                                comp.component.data.hasOwnProperty('url') && !$.isEmptyObject(comp.component.data.url)) {
-                                comp.component.data.url = base_url.concat(comp.component.data.url, '/', uuid);
-                            }
-                            return container;
+            $.jsonRpc.request(options_url, 'call', {}).then(function(_options) {
+                var options = JSON.parse(_options);
+                var hooks = {
+                    'addComponent': function(container, comp, parent) {
+                        if (comp.hasOwnProperty('component') && comp.component.hasOwnProperty('data') &&
+                            comp.component.data.hasOwnProperty('url') && !$.isEmptyObject(comp.component.data.url)) {
+                            comp.component.data.url = base_url.concat(comp.component.data.url, '/', uuid);
                         }
+                        return container;
+                    }
+                };
+                options['hooks'] = hooks;
+
+                Formio.createForm(document.getElementById('formio_form'), schema, options).then(function(form) {
+                    // Language
+                    if ('language' in options) {
+                        form.language = options['language'];
+                    }
+                    window.setLanguage = function(lang) {
+                        form.language = lang;
                     };
-                    options['hooks'] = hooks;
-
-                    Formio.createForm(document.getElementById('formio_form'), schema, options).then(function(form) {
-                        // Language
-                        if ('language' in options) {
-                            form.language = options['language'];
-                        }
-                        window.setLanguage = function(lang) {
-                            form.language = lang;
-                        };
-                        
-                        // Events
-                        form.on('submit', function(submission) {
-                            ajax.jsonRpc(submit_url, 'call', {
-                                'uuid': uuid,
-                                'data': submission.data
-                            }).then(function() {
-                                form.emit('submitDone', submission);
-                            });
-                        });
-                        form.on('submitDone', function(submission) {
-                            window.parent.postMessage('formioSubmitDone', base_url);
-                        });
-                        // Set the Submission (data)
-                        // https://github.com/formio/formio.js/wiki/Form-Renderer#setting-the-submission
-                        ajax.jsonRpc(submission_url, 'call', {}).then(function(result) {
-                            if (!$.isEmptyObject(result)) {
-                                form.submission = {'data': JSON.parse(result)};
-                            }
+                    
+                    // Events
+                    form.on('submit', function(submission) {
+                        $.jsonRpc.request(submit_url, 'call', {
+                            'uuid': uuid,
+                            'data': submission.data
+                        }).then(function() {
+                            form.emit('submitDone', submission);
                         });
                     });
+                    form.on('submitDone', function(submission) {
+                        window.parent.postMessage('formioSubmitDone', base_url);
+                    });
+                    // Set the Submission (data)
+                    // https://github.com/formio/formio.js/wiki/Form-Renderer#setting-the-submission
+                    $.jsonRpc.request(submission_url, 'call', {}).then(function(result) {
+                        if (!$.isEmptyObject(result)) {
+                            form.submission = {'data': JSON.parse(result)};
+                        }
+                    });
                 });
-            }
-        });
+            });
+        }
     });
 });
