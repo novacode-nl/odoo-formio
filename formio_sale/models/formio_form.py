2f0ea5b9975dd2a523891b6fd73538028e041668
yannik <yannik@lieblinger.de>
2020-03-19 21:10:39 +0100

    [FIX] Main _compute_res_fields always returned false, in submodule (partner, crm).

--- formio_sale/models/formio_form.py
+++ formio_sale/models/formio_form.py
@@ -15,27 +15,25 @@ class Form(models.Model):
 
     @api.depends('res_model_id', 'res_id')
     def _compute_res_fields(self):
-        compute = super(Form, self)._compute_res_fields()
-        if not compute:
-            return False
+        super(Form, self)._compute_res_fields()
+        if self._context.get('active_model') == 'sale.order':
+            for r in self:
+                if r.res_model == 'sale.order' and r.res_id:
+                    order = self.env['sale.order'].search([('id', '=', r.res_id)])
+                    r.sale_order_id = order.id
+                    r.res_partner_id = order.partner_id.id
+                    if order.state in ('draft', 'sent'):
+                        action = self.env.ref('sale.action_quotations')
+                    else:
+                        action = self.env.ref('sale.action_orders')
 
-        for r in self:
-            if r.res_model == 'sale.order' and r.res_id:
-                order = self.env['sale.order'].search([('id', '=', r.res_id)])
-                r.sale_order_id = order.id
-                r.res_partner_id = order.partner_id.id
-                if order.state in ('draft', 'sent'):
-                    action = self.env.ref('sale.action_quotations')
-                else:
-                    action = self.env.ref('sale.action_orders')
-
-                url = '/web?#id={id}&view_type=form&model={model}&action={action}'.format(
-                    id=r.res_id,
-                    model='sale.order',
-                    action=action.id)
-                r.res_act_window_url = url
-                r.res_name = order.name
-                r.res_info = get_field_selection_label(order, 'state')
+                    url = '/web?#id={id}&view_type=form&model={model}&action={action}'.format(
+                        id=r.res_id,
+                        model='sale.order',
+                        action=action.id)
+                    r.res_act_window_url = url
+                    r.res_name = order.name
+                    r.res_info = get_field_selection_label(order, 'state')
 
     @api.onchange('builder_id')
     def _onchange_builder(self):
