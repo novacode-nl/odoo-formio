888c77c1d83d6c5f83d6913adf06ee36e624a0ed
Bob Leers <bob@novacode.nl>
2020-03-29 22:24:05 +0200

    [UPD] website_formio: Remove useless submit_done_page.

--- website_formio/models/formio_builder.py
+++ website_formio/models/formio_builder.py
@@ -7,16 +7,11 @@ from odoo import api, fields, models
 class Builder(models.Model):
     _inherit = 'formio.builder'
 
-    submit_done_page_id = fields.Many2one('website.page', domain=[('website_published', '=', True), ('url', '!=', '/')])
-    portal_submit_done_page_id = fields.Many2one('website.page', domain=[('website_published', '=', True), ('url', '!=', '/')])
+    portal_submit_done_page_id = fields.Many2one('website.page', domain=[('is_published', '=', True), ('url', '!=', '/')])
 
     @api.model
     def create(self, vals):
-        submit_done_page_id = vals.get('submit_done_page_id')
         portal_submit_done_page_id = vals.get('portal_xsubmit_done_page_id')
-        if submit_done_page_id:
-            submit_done_page = self.env['website.page'].browse(submit_done_page_id)
-            vals['submit_done_url'] = submit_done_page.url
         if portal_submit_done_page_id:
             portal_submit_done_page = self.env['website.page'].browse(portal_submit_done_page_id)
             vals['portal_submit_done_url'] = portal_submit_done_page.url
@@ -24,22 +19,11 @@ class Builder(models.Model):
 
     @api.multi
     def write(self, vals):
-        submit_done_page_id = vals.get('submit_done_page_id')
         portal_submit_done_page_id = vals.get('portal_submit_done_page_id')
-        if submit_done_page_id:
-            submit_done_page = self.env['website.page'].browse(submit_done_page_id)
-            vals['submit_done_url'] = submit_done_page.url
         if portal_submit_done_page_id:
             portal_submit_done_page = self.env['website.page'].browse(portal_submit_done_page_id)
             vals['portal_submit_done_url'] = portal_submit_done_page.url
         return super(Builder, self).write(vals)
-
-    @api.onchange('submit_done_page_id')
-    def _onchange_submit_done_page(self):
-        if not self.submit_done_page_id:
-            self.submit_done_url = False
-        else:
-            self.submit_done_url = self.submit_done_page_id.url
 
     @api.onchange('portal_submit_done_page_id')
     def _onchange_portal_submit_done_page(self):
