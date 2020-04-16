940002542cc849109f7b27b475098279d32feab3
Bob Leers <bob@novacode.nl>
2020-04-16 21:38:19 +0200

    [ADD] formio_data_api (initial version).

---
+++ formio_data_api/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright Nova Code (http://www.novacode.nl)
+# See LICENSE file for full licensing details.
+
+{
+    'name': 'Form.io Data API',
+    'summary': 'Pythonic API to Form.io data.',
+    'version': '0.1',
+    'author': 'Nova Code',
+    'website': 'https://www.novacode.nl',
+    'license': 'LGPL-3',
+    'category': 'Extra Tools',
+    'depends': ['formio'],
+    'data': [],
+    'external_dependencies': {
+        'python': ['formiodata'],
+    },
+    'application': False,
+    'images': [
+        'static/description/banner.png',
+    ],
+    'description': """
+Form.io Data API
+================
+
+"""
+}
