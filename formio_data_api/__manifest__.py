d02e6388913b48fd95276f5e3ee0ea82071090f8
Bob Leers <bob@novacode.nl>
2020-04-30 23:04:26 +0200

    formio_data_api: Bump version 0.2.

--- formio_data_api/__manifest__.py
+++ formio_data_api/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Form.io Data API',
     'summary': 'Python API for Form.io data (form submission).',
-    'version': '0.1',
+    'version': '0.2',
     'author': 'Nova Code',
     'website': 'https://www.novacode.nl',
     'license': 'LGPL-3',
