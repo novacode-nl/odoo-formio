8f40264c4b045c3d1ebae63eefcf09461c9450a4
Bob Leers <bob@novacode.nl>
2020-05-01 12:13:27 +0200

    [ADD] formio_report_qweb: Reports configuration per Builder (show not implemented components)

---
+++ formio_report_qweb/models/__init__.py
@@ -0,0 +1,7 @@
+# Copyright Nova Code (http://www.novacode.nl)
+# See LICENSE file for full licensing details.
+
+from . import formio_builder
+from . import formio_builder_report
+from . import formio_form
+from . import ir_actions_report
---
+++ formio_report_qweb/models/ir_actions_report.py
@@ -0,0 +1,11 @@
+# Copyright Nova Code (http://www.novacode.nl)
+# See LICENSE file for full licensing details.
+
+from odoo import fields, models
+
+
+class IrActionsReportXml(models.Model):
+    _inherit = 'ir.actions.report'
+
+    formio_builder_report_ids = fields.One2many(
+        'formio.builder.report', 'ir_actions_report_id', string='Formio Builder Reports')
