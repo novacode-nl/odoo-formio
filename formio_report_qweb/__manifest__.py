4ee788271820419a60ab1624de74b2e7f419d30f
Bob Leers <bob@novacode.nl>
2020-05-02 15:55:39 +0200

    [IMP] formio: Module info (textual) improvements.

--- formio_report_qweb/__manifest__.py
+++ formio_report_qweb/__manifest__.py
@@ -2,15 +2,15 @@
 # See LICENSE file for full licensing details.
 
 {
-    "name": "Form.io Reports Generator",
-    'summary': 'Out-of-the-box printing Form.io webforms to (QWeb) formats e.g. PDF',
-    "version": "0.4",
+    'name': 'Form.io Reporting Engine',
+    'summary': 'Generate a (PDF) report for every Form',
+    'version': '0.4',
     'author': 'Nova Code',
     'website': 'https://www.novacode.nl',
-    "license": "LGPL-3",
+    'license': 'LGPL-3',
     'category': 'Extra Tools',
-    "depends": ["formio", "formio_data_api"],
-    "data": [
+    'depends': ['formio', 'formio_data_api'],
+    'data': [
         'report/formio_form_report_views.xml',
         'report/report_formio_form.xml',
         'report/report_formio_form_components.xml',
@@ -18,7 +18,7 @@
         'views/formio_builder_views.xml',
         'views/formio_builder_report_views.xml'
     ],
-    "application": False,
+    'application': False,
     'images': [
         'static/description/banner.png',
     ],
