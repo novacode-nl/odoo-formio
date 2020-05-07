7bef8592b3406cc275ef1abf0c7f5e17ca447598
Bob Leers <bob@novacode.nl>
2020-05-07 15:32:48 +0200

    [PORT] formio_partner v13

--- formio_partner/models/partner.py
+++ formio_partner/models/partner.py
@@ -20,7 +20,6 @@ class ResPartner(models.Model):
         model_id = self.env.ref('base.model_res_partner').id
         self.formio_this_model_id = model_id
 
-    @api.multi
     def action_formio_forms(self):
         self.ensure_one()
         res_model_id = self.env.ref('base.model_res_partner').id
