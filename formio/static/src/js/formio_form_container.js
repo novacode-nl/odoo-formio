07f906e3f3a383719ec17182576adbfee62a94e1
Bob Leers <bob@novacode.nl>
2020-05-24 20:50:37 +0200

    [FIX] formio: Portal iFrameResize not working. jQuery wasn't DOM loaded.
    This addresses #35.

---
+++ formio/static/src/js/formio_form_container.js
@@ -0,0 +1,19 @@
+// Copyright Nova Code (http://www.novacode.nl)
+// See LICENSE file for full licensing details.
+
+$(document).ready(function() {
+    iFrameResize({}, '.formio_form_embed');
+
+    window.addEventListener('message', function(event) {
+        var base_url = window.location.protocol + '//' + window.location.host;
+        if (event.origin == base_url && event.data == 'formioSubmitDone') {
+            var portal_submit_done_url = document.getElementById('portal_submit_done_url').value;
+            if (portal_submit_done_url && portal_submit_done_url.length > 0) {
+                window.location = portal_submit_done_url;
+            }
+            else {
+                window.location.reload();
+            }
+        }
+    }, false);
+});
