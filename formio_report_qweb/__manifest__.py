d1e2764ebee60e0838d0bfa71eb413ecad64f8a7
Bob Leers <bob@novacode.nl>
2020-05-29 13:34:54 +0200

    [UPD] formio_report_qweb: Add layout components (table, tabs).

--- formio_report_qweb/__manifest__.py
+++ formio_report_qweb/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Form.io Reports Generator',
     'summary': 'Generate (PDF) reports for every Form',
-    'version': '0.6',
+    'version': '0.7',
     'author': 'Nova Code',
     'website': 'https://www.novacode.nl',
     'license': 'LGPL-3',
