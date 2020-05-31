9ce8d7db5ac1f4dab456aaefa1e9cf733d7a4779
Bob Leers <bob@novacode.nl>
2020-05-31 14:29:17 +0200

    [UPD] formio: Comment the (override) unlink of formio.res.model

--- formio/models/formio_res_model.py
+++ formio/models/formio_res_model.py
@@ -14,9 +14,9 @@ class ResModel(models.Model):
     ir_model_id = fields.Many2one('ir.model', string='Model')
     module_dependency = fields.Boolean(readonly=True)
 
-    @api.multi
-    def unlink(self):
-        if not self.module_dependency:
-            return super(ResModel, self).unlink()
-        else:
-            raise UserError(_("Can't unlink because this record is a module dependency!"))
+    # @api.multi
+    # def unlink(self):
+    #     if not self.module_dependency:
+    #         return super(ResModel, self).unlink()
+    #     else:
+    #         raise UserError(_("Can't unlink because this record is a module dependency!"))
