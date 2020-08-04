abf33db19fa86b1aa7ced4d9b183a4cc97a964f1
Bob Leers <bob@novacode.nl>
2020-08-04 14:24:50 +0200

    [UPD] formio_data_api: ETL the Odoo model name into Forms (API Property Name: OdooModel).

--- formio_data_api/__manifest__.py
+++ formio_data_api/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Form.io Data API',
     'summary': 'Python API for Form.io data (builder, form/submission).',
-    'version': '0.3',
+    'version': '0.4',
     'author': 'Nova Code',
     'website': 'https://www.novacode.nl',
     'license': 'LGPL-3',
