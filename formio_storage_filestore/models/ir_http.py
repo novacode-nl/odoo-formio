01fba28a92acbc475040dfcc38b85d1d5e1c0268
Bob Leers <bob@novacode.nl>
2021-01-14 16:02:22 +0100

    [PORT] formio_storage_filestore: ir.http _authenticate() func endpoint is request path.

--- formio_storage_filestore/models/ir_http.py
+++ formio_storage_filestore/models/ir_http.py
@@ -14,29 +14,20 @@ class IrHttp(models.AbstractModel):
     _inherit = 'ir.http'
 
     @classmethod
-    def _authenticate(cls, endpoint):
-        """
-        :param endpoint str: the endpoint config, for example (endpoint.__dict_):
-            {'method': <bound method FormioStorageFilestoreController.form_file_post of <odoo.addons.formio_storage_filestore.controllers.main.FormioStorageFilestoreController object at 0x7fa358108520>>,
-             'original': <function FormioStorageFilestoreController.form_file_post at 0x7fa35990a1f0>,
-             'routing': {'type': 'http', 'auth': 'user', 'methods': ['POST'], 'routes': ['/formio/storage/filestore'], 'csrf': False},
-             'arguments': {}
-            }
-        """
-        if '/formio/storage/filestore' in endpoint.routing.get('routes', []):
-            auth_method = endpoint.routing["auth"]
+    def _authenticate(cls, auth_method='user'):
+        if request.httprequest.path == '/formio/storage/filestore':
             log = {
-                'routes': endpoint.routing.get('routes'),
+                'request_path': request.httprequest.path,
                 'auth_method': auth_method,
                 'request_user_id': request.env.user.id
             }
             _logger.debug(log)
 
             if auth_method == 'user' and request.env.user:
-                auth_method = super(IrHttp, cls)._authenticate(endpoint)
+                auth_method = super(IrHttp, cls)._authenticate(auth_method)
                 return auth_method
             else:
                 _logger.debug('Allow public /formio/storage/filestore , become: auth=public')
                 return 'public'
         else:
-            return super(IrHttp, cls)._authenticate(endpoint)
+            return super(IrHttp, cls)._authenticate(auth_method)
