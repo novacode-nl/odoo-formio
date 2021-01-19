7aadfe71c2502a741a0a2692a5dc8cf442a811ee
Bob Leers <bob@novacode.nl>
2021-01-19 21:50:19 +0100

    [FIX, IMP] website_formio: Show the complete (resized) Form, after dropping on the webpage!
    Only list published Form Builders in the drag&drop selectbox (Form chooser).

--- website_formio/__manifest__.py
+++ website_formio/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Forms | Website (Snippet etc)',
     'summary': 'Forms Snippet, Other website integration',
-    'version': '1.4',
+    'version': '1.5',
     'license': 'LGPL-3',
     'author': 'Nova Code',
     'website': 'https://www.novacode.nl',
