ac3a0891c1c007667a359191c0b90e8e9482e8e3
Bob Leers <bob@novacode.nl>
2021-02-17 11:56:21 +0100

    [FIX] formio_storage_filestore: Deletion of other attachments was impossible due to a rigorous record rule, introduced in 0.4
    - Removed the Record Rule (XML ID): ir_attachment_unlink_formio_form_storage_filestore
    - Implement unlink method in the ir.attachment model, which raises an error (message) upon deletion of attachments beloning to submitted Form(s).
    - Implement unlink method in the formio.form model, which handles (kinda) cascade deletion of attachments upon deleting Form(s).

--- formio_storage_filestore/models/ir_attachment.py
+++ formio_storage_filestore/models/ir_attachment.py
@@ -1,7 +1,8 @@
 # Copyright Nova Code (http://www.novacode.nl)
 # See LICENSE file for full licensing details.
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 
 class IrAttachment(models.AbstractModel):
@@ -17,3 +18,12 @@ class IrAttachment(models.AbstractModel):
             ('res_id', '=', False)
         ]
         self.search(domain).unlink()
+
+    def unlink(self):
+        for attach in self:
+            if attach.formio_storage_filestore_user_id and attach.res_model == 'formio.form' and attach.res_id:
+                msg = _("It's not allowed to delete an attachment which belongs to a Form (formio.form).") + \
+                '\n- ' + _('Attachment ID, name: %s, %s') % (attach.id, attach.name) + \
+                '\n- ' + _('Form (formio.form) ID: %s') % attach.res_id
+                raise UserError(msg)
+        return super(IrAttachment, self).unlink()
