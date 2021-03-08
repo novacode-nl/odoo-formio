f5b3d176845c0aede53beca2de0d133ff7d1647d
Bob Leers <bob@novacode.nl>
2021-03-08 14:16:30 +0100

    [FIX] formio: Ordering of "Form.io Version" in (Many2one) selection fields.
    Solves #127

---
+++ formio/migrations/13.0.8.20/post-update_formio_version_sequence.py
@@ -0,0 +1,9 @@
+# Copyright Nova Code (http://www.novacode.nl)
+# See LICENSE file for full licensing details.
+
+from odoo import api, registry, SUPERUSER_ID
+
+def migrate(cr, version):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    formio_version = env['formio.version']
+    formio_version._update_sequence()
--- formio/models/formio_version.py
+++ formio/models/formio_version.py
@@ -1,17 +1,20 @@
 # Copyright Nova Code (http://www.novacode.nl)
 # See LICENSE file for full licensing details.
 
-from odoo import api, fields, models, _
+from pkg_resources import parse_version
+
+from odoo import api, fields, models
 
 
 class Version(models.Model):
     _name = 'formio.version'
     _description = 'Formio Version'
-    _order = 'name DESC'
+    _order = 'sequence DESC'
 
     name = fields.Char(
         "Name", required=True, track_visibility='onchange',
         help="""Form.io release/version.""")
+    sequence = fields.Integer()
     description = fields.Text("Description")
     translations = fields.Many2many('formio.translation', string='Translations')
     assets = fields.One2many('formio.version.asset', 'version_id', string='Assets')
@@ -26,3 +29,27 @@ class Version(models.Model):
         domain = [('formio_version_id', '=', self.ids)]
         self.env['formio.version.github.tag'].search(domain).write({'state': 'available'})
         return super(Version, self).unlink()
+
+    @api.model
+    def create(self, vals):
+        res = super(Version, self).create(vals)
+        self._update_versions_sequence()
+        return res
+
+    def write(self, vals):
+        res = super(Version, self).write(vals)
+        if 'name' in vals:
+            self._update_versions_sequence()
+        return res
+
+    @api.model
+    def _update_versions_sequence(self):
+        versions = self.search([])
+        names = versions.mapped('name')
+        names = sorted(names, key=parse_version)
+        seq = 0
+        for name in names:
+            seq += 1
+            version = versions.filtered(lambda r: r.name == name)[0]
+            version.sequence = seq
+        
