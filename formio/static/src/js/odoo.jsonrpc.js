da9086a9be6c2ae4853017f20694cf776f8eaa96
Bob Leers <bob@novacode.nl>
2021-03-16 08:01:28 +0100

    [IMP] formio: Improve JsonRPC client using Odoo's JsonRpc internals (from web.ajax).

--- formio/static/src/js/odoo.jsonrpc.js
+++ formio/static/src/js/odoo.jsonrpc.js
@@ -1,7 +1,7 @@
 (function($, undefined) {
     $.extend({
         jsonRpc: {
-            genericJsonRpc: function(fct_name, params, settings, fct) {
+            _genericJsonRpc: function(fct_name, params, settings, fct) {
                 // Copied from addons/web/static/src/js/core/ajax.js
                 // with slightly modifications.
                 var data = {
@@ -11,56 +11,78 @@
                     id: Math.floor(Math.random() * 1000 * 1000 * 1000)
                 };
                 var xhr = fct(data);
-                var result = xhr.pipe(function(result) {
+                var result = xhr.then(function(result) {
                     if (result.error !== undefined) {
-                        console.error(
-                            "Server application error\n",
-                            "Error code:", result.error.code, "\n",
-                            "Error message:", result.error.message, "\n",
-                            "Error data message:\n", result.error.data.message, "\n",
-                            "Error data debug:\n", result.error.data.debug
-                        );
-                        return $.Deferred().reject("server", result.error);
+                        if (result.error.data.arguments[0] !== "bus.Bus not available in test mode") {
+                            console.debug(
+                                "Server application error\n",
+                                "Error code:", result.error.code, "\n",
+                                "Error message:", result.error.message, "\n",
+                                "Error data message:\n", result.error.data.message, "\n",
+                                "Error data debug:\n", result.error.data.debug
+                            );
+                        }
+                        return Promise.reject({type: "server", error: result.error});
                     } else {
                         return result.result;
                     }
                 }, function() {
                     //console.error("JsonRPC communication error", _.toArray(arguments));
-                    var def = $.Deferred();
-                    return def.reject.apply(def, ["communication"].concat(_.toArray(arguments)));
+                    var reason = {
+                        type: 'communication',
+                        error: arguments[0],
+                        textStatus: arguments[1],
+                        errorThrown: arguments[2],
+                    };
+                    return Promise.reject(reason);
                 });
-                // FIXME: jsonp?
-                result.abort = function () { if (xhr.abort) xhr.abort(); };
-                
-                var p = result.then(function (result) {
-                    return result;
-                }, function (type, error, textStatus, errorThrown) {
-                    if (type === "server") {
-                        return $.Deferred().reject(error, $.Event());
-                    } else {
-                        var nerror = {
-                            code: -32098,
-                            message: "XmlHttpRequestError " + errorThrown,
-                            data: {
-                                type: "xhr"+textStatus,
-                                debug: error.responseText,
-                                objects: [error, errorThrown]
-                            },
-                        };
-                        return $.Deferred().reject(nerror, $.Event());
-                    }
-                });
-                return p.fail(function () { // Allow deferred user to disable rpc_error call in fail
-                    p.fail(function (error, event) {
-                        console.log(error);
+
+                var rejection;
+                var promise = new Promise(function (resolve, reject) {
+                    rejection = reject;
+
+                    result.then(function (result) {
+                        resolve(result);
+                    }, function (reason) {
+                        var type = reason.type;
+                        var error = reason.error;
+                        var textStatus = reason.textStatus;
+                        var errorThrown = reason.errorThrown;
+                        if (type === "server") {
+                            reject({message: error, event: $.Event()});
+                        } else {
+                            var nerror = {
+                                code: -32098,
+                                message: "XmlHttpRequestError " + errorThrown,
+                                data: {
+                                    type: "xhr"+textStatus,
+                                    debug: error.responseText,
+                                    objects: [error, errorThrown],
+                                    arguments: [reason || textStatus]
+                                },
+                            };
+                            reject({message: nerror, event: $.Event()});
+                        }
                     });
                 });
+
+                // FIXME: jsonp?
+                promise.abort = function () {
+                    rejection({
+                        message: "XmlHttpRequestError abort",
+                        event: $.Event('abort')
+                    });
+                    if (xhr.abort) {
+                        xhr.abort();
+                    }
+                };
+                return promise;
             },
             
             request: function(url, fct_name, params, settings) {
                 // original function is jsonRpc
                 settings = settings || {};
-                return this.genericJsonRpc(fct_name, params, settings, function(data) {
+                return this._genericJsonRpc(fct_name, params, settings, function(data) {
                     return $.ajax(url, $.extend({}, settings, {
                         url: url,
                         dataType: 'json',
