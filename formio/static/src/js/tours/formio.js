a4f6e7bb39c13c78bfbe4b17e72f29e94a48d355
Bob Leers <bob@novacode.nl>
2021-05-12 09:31:36 +0200

    [IMP] formio: Add tour on the Form Builder button.

---
+++ formio/static/src/js/tours/formio.js
@@ -0,0 +1,19 @@
+odoo.define('formio.tour', function(require) {
+    "use strict";
+
+    var core = require('web.core');
+    var tour = require('web_tour.tour');
+
+    var _t = core._t;
+
+    tour.register("formio_tour", {
+        url: "/web",
+        rainbowMan: false,
+        sequence: 20,
+    }, [tour.stepUtils.showAppsMenuItem(), {
+        trigger: "button[name='action_view_formio']",
+        content: _t("After save, click here (or the view switch) to open the Form Builder."),
+        position: "bottom",
+    }]);
+
+});
