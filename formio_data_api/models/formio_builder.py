9f8f8826456c130018dcdd26b1ba5ed4a273a74b
Bob Leers <bob@novacode.nl>
2021-05-26 12:07:33 +0200

    [ADD] formio_data_api (1.0): formio.builder (model) with validation in action_current.

---
+++ formio_data_api/models/formio_builder.py
@@ -0,0 +1,46 @@
+# Copyright Nova Code (http://www.novacode.nl)
+# See LICENSE file for full licensing details.
+
+import logging
+
+from formiodata.builder import Builder
+
+from odoo import models, _
+from odoo.exceptions import ValidationError
+
+
+_logger = logging.getLogger(__name__)
+
+
+class FormioBuilder(models.Model):
+    _inherit = 'formio.builder'
+
+    def _component_api_keys(self):
+        return ['model_field', 'res_field', 'user_field']
+
+    def action_current(self):
+        self._validate_component_api_properties()
+        super(FormioBuilder, self).action_current()
+    
+    def _validate_component_api_properties(self):
+        builder = Builder(self.schema)
+
+        for comp_name, component in builder.input_components.items():
+            properties = component.properties or {}
+            found_prefix_keys = {prefix: False for prefix in self._component_api_keys()}
+            keys = self._component_api_keys()
+
+            for prop_key, prop_val in properties.items():
+                if any([prop_key == prefix for prefix in self._component_api_keys()]):
+                    found_prefix_keys[prop_key] = True
+
+            # TODO check all components and collect errors (log/raise only once)
+            if sum([1 for check in found_prefix_keys.values() if check == True]) > 1:
+                msg = _('Incorrect or conflicting "API Custom Properties" for Form Component, with:\n'
+                        '- Label: %s\n'
+                        '- Key: %s\n\n' \
+                        'Custom Properties:\n' \
+                        '%s')
+
+                display_error = _(msg) % (component.label, component.key, properties)
+                raise ValidationError(display_error)
