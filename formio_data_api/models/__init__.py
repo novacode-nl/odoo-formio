a191ea640bcd6147457b58287d545732c10fb855
Bob Leers <bob@novacode.nl>
2021-05-26 11:55:26 +0200

    [UPD] formio_data_api (1.0): Change component ETL API to load and prefil data from Odoo field(s).
    This is a major change that greatly improves ease of use, flexibility
    and new features to come.
    
    The previous API/usage (in Property Name, aka the component key) is
    still supported, but deprecated and logs a DEPRECATION message.

--- formio_data_api/models/__init__.py
+++ formio_data_api/models/__init__.py
@@ -1,5 +1,6 @@
 # Copyright Nova Code (http://www.novacode.nl)
 # See LICENSE file for full licensing details.
 
+from . import formio_builder
 from . import formio_form
 from . import mail_render_mixin
