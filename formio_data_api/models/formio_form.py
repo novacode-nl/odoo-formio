56542926bdaa13cf6c7afa527db3c85663170227
Bob Leers <bob@novacode.nl>
2021-07-01 08:04:40 +0200

    [UPD] formio_data_api: Renaming (v1.2) ETL API context['values'] TO 'value'.

--- formio_data_api/models/formio_form.py
+++ formio_data_api/models/formio_form.py
@@ -278,7 +278,7 @@ class FormioForm(models.Model):
                         eval_context = self._get_formio_eval_context(comp)
                         # nocopy allows to return 'value'
                         safe_eval.safe_eval(api.code, eval_context, mode="exec", nocopy=True)
-                        context_values = eval_context.get('values')
+                        context_values = eval_context.get('value')
                         value = context_values.get(prop_value)
                         if prop_value_obj:
                             # TODO-2: refactor DRY
@@ -384,7 +384,7 @@ class FormioForm(models.Model):
             :returns: dict -- evaluation context given to safe_eval
         """
         return {
-            'values': {},
+            'value': {},
             'env': self.env,
             'component': component,
             'record': self,
