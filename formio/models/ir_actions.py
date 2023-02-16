e83ae52bfd204e6cd4a2287037948eb39f8ae049
bobslee <bob@novacode.nl>
2023-02-16 12:13:58 +0100

    formio: Fix constrains (formio_code) in ir.actions.server

--- formio/models/ir_actions.py
+++ formio/models/ir_actions.py
@@ -17,8 +17,9 @@ class ServerAction(models.Model):
     @api.constrains('formio_code')
     def constaint_checkq_formio_code(self):
         self.ensure_one
-        if re.search(r"[^a-zA-Z0-9_-]", self.formio_code) is not None:
-            raise ValidationError(_('Forms Code is invalid. Use ASCII letters, digits, "-" or "_".'))
+        if self.formio_code:
+            if re.search(r"[^a-zA-Z0-9_-]", self.formio_code) is not None:
+                raise ValidationError(_('Forms Code is invalid. Use ASCII letters, digits, "-" or "_".'))
 
     @api.constrains('formio_code')
     def _constraint_unique_formio_code(self):
@@ -27,5 +28,5 @@ class ServerAction(models.Model):
             if self.search_count(domain) > 1:
                 msg = _(
                     'A Server Action with Forms Code "%s" already exists.\nForms Code should be unique.'
-                ) % (rec.name)
+                ) % (rec.formio_code)
                 raise ValidationError(msg)
