5dd329e6ec27a19af7fb6a31dff5a20b9ceefdfd
bobslee <bob@novacode.nl>
2023-02-23 11:25:33 +0100

    formio: Fix ir.actions constraint_unique_formio_ref

--- formio/models/ir_actions.py
+++ formio/models/ir_actions.py
@@ -25,7 +25,7 @@ class ServerAction(models.Model):
     def _constraint_unique_formio_ref(self):
         for rec in self:
             domain = [("formio_ref", "=", rec.formio_ref)]
-            if self.search_count(domain) > 1:
+            if self.formio_ref and self.search_count(domain) > 1:
                 msg = _(
                     'A Server Action with Forms Ref "%s" already exists.\nForms Ref should be unique.'
                 ) % (rec.formio_ref)
