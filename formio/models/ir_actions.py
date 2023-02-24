c0b92028f3a90cc92ae3de1d606133737bdc9515
bobslee <bob@novacode.nl>
2023-02-24 18:15:10 +0100

    [IMP] formio: onchange server-action model_id: provide formio_ref uuid.
    A UUID is a sensible default value for the formio_ref field.

--- formio/models/ir_actions.py
+++ formio/models/ir_actions.py
@@ -2,6 +2,7 @@
 # See LICENSE file for full licensing details.
 
 import re
+import uuid
 
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
@@ -11,8 +12,17 @@ class ServerAction(models.Model):
     _inherit = "ir.actions.server"
 
     formio_ref = fields.Char(
-        string="Forms Ref", help="Identifies a server action with related form builder."
+        string="Forms Ref",
+        help="Identifies a server action with related form builder.",
     )
+
+    @api.onchange('model_id')
+    def _onchange_formio_ref(self):
+        form_model = self.env.ref('formio.model_formio_form')
+        if self.model_id.id == form_model.id and not self.formio_ref:
+            self.formio_ref = str(uuid.uuid4())
+        elif self.model_id.id != form_model.id:
+            self.formio_ref = False
 
     @api.constrains('formio_ref')
     def constaint_check_formio_ref(self):
