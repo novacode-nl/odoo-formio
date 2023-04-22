772161e3bb95f380378434e699f91c661fa5868c
bobslee <bob@novacode.nl>
2023-04-22 08:25:56 +0200

    formio: Remove obsolete migrations.

--- formio/migrations/13.0.8.20/post-update_formio_version_sequence.py
+++
@@ -1,9 +0,0 @@
-# Copyright Nova Code (http://www.novacode.nl)
-# See LICENSE file for full licensing details.
-
-from odoo import api, registry, SUPERUSER_ID
-
-def migrate(cr, version):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    formio_version = env['formio.version']
-    formio_version._update_versions_sequence()
