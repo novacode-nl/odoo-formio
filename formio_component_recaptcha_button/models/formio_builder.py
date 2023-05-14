17b8294c40b25f76d87cf6bcb38d2c54df0539f4
bobslee <bob@novacode.nl>
2023-05-14 16:10:37 +0200

    [MIG] formio_component_recaptcha_button: remove obsolete/commented code

--- formio_component_recaptcha_button/models/formio_builder.py
+++ formio_component_recaptcha_button/models/formio_builder.py
@@ -10,15 +10,12 @@ class Builder(models.Model):
     _inherit = 'formio.builder'
 
     component_recaptcha_button_site_key = fields.Char(string='(odoo) reCAPTCHA Site Key', compute='_compute_recaptcha_button')
-    # component_recaptcha_button_secret_key = fields.Char(string='(odoo) reCAPTCHA Secret Key', compute='_compute_recaptcha_button')
     component_recaptcha_button_active = fields.Boolean(string='(odoo) reCAPTCHA Active')
 
     def _compute_recaptcha_button(self):
         Config = self.env['ir.config_parameter'].sudo()
         site_key = Config.get_param('recaptcha_public_key')
-        # secret_key = Config.get_param('formio_recaptcha_button.secret_key')
         self.component_recaptcha_button_site_key = site_key
-        # self.component_recaptcha_button_secret_key = secret_key
 
     def write(self, vals):
         other_builders = self
