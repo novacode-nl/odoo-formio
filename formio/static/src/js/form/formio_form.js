ff8895129a4efb39771eae0bb7127bff368ec2b0
Peter Bex <peter@more-magic.net>
2023-05-28 14:14:32 +0200

    Call form's beforeSubmit in wizard draft autosave mode
    This allows complex components to serialize their data on the form
    before submitting it.

--- formio/static/src/js/form/formio_form.js
+++ formio/static/src/js/form/formio_form.js
@@ -63,6 +63,26 @@ export class OdooFormioForm extends Component {
         }
     }
 
+    wizardStateChange (form, submission) {
+        this.resetParentIFrame();
+        // readOnly check also applies in server endpoint
+        const readOnly = 'readOnly' in this.options && this.options['readOnly'] == true;
+        if (this.params['wizard_on_change_page_save_draft'] && !readOnly) {
+	    form.beforeSubmit();
+            const data = {'data': form.data, 'saveDraft': true};
+            if (this.formUuid) {
+                data['form_uuid'] = this.formUuid;
+            }
+            $.jsonRpc.request(this.submitUrl, 'call', data).then(function(submission) {
+                if (typeof(submission) != 'undefined') {
+                    // Set properties to instruct the next calls to save (draft) the current form.
+                    this.formUuid = submission.form_uuid;
+                    this.submitUrl = this.wizardSubmitUrl + this.formUuid + '/submit';
+                }
+            });
+        }
+    }
+
     loadForm() {
         const self = this;
         let configUrl = self.configUrl;
@@ -268,62 +288,9 @@ export class OdooFormioForm extends Component {
             });
 
             // wizard
-            form.on('wizardPageSelected', function(submission) {
-                self.resetParentIFrame();
-                // readOnly check also applies in server endpoint
-                const readOnly = 'readOnly' in self.options && self.options['readOnly'] == true;
-                if (self.params['wizard_on_change_page_save_draft'] && !readOnly) {
-                    const data = {'data': form.data, 'saveDraft': true};
-                    if (self.formUuid) {
-                        data['form_uuid'] = self.formUuid;
-                    }
-                    $.jsonRpc.request(self.submitUrl, 'call', data).then(function(submission) {
-                        if (typeof(submission) != 'undefined') {
-                            // Set properties to instruct the next calls to save (draft) the current form.
-                            self.formUuid = submission.form_uuid;
-                            self.submitUrl = self.wizardSubmitUrl + self.formUuid + '/submit';
-                        }
-                    });
-                }
-            });
-
-            form.on('prevPage', function(submission) {
-                self.resetParentIFrame();
-                // readOnly check also applies in server endpoint
-                const readOnly = 'readOnly' in self.options && self.options['readOnly'] == true;
-                if (self.params['wizard_on_change_page_save_draft'] && !readOnly) {
-                    const data = {'data': form.data, 'saveDraft': true};
-                    if (self.formUuid) {
-                        data['form_uuid'] = self.formUuid;
-                    }
-                    $.jsonRpc.request(self.submitUrl, 'call', data).then(function(submission) {
-                        if (typeof(submission) != 'undefined') {
-                            // Set properties to instruct the next calls to save (draft) the current form.
-                            self.formUuid = submission.form_uuid;
-                            self.submitUrl = self.wizardSubmitUrl + self.formUuid + '/submit';
-                        }
-                    });
-                }
-            });
-
-            form.on('nextPage', function(submission) {
-                self.resetParentIFrame();
-                // readOnly check also applies in server endpoint
-                const readOnly = 'readOnly' in self.options && self.options['readOnly'] == true;
-                if (self.params['wizard_on_change_page_save_draft'] && !readOnly) {
-                    const data = {'data': form.data, 'saveDraft': true};
-                    if (self.formUuid) {
-                        data['form_uuid'] = self.formUuid;
-                    }
-                    $.jsonRpc.request(self.submitUrl, 'call', data).then(function(submission) {
-                        if (typeof(submission) != 'undefined') {
-                            // Set properties to instruct the next calls to save (draft) the current form.
-                            self.formUuid = submission.form_uuid;
-                            self.submitUrl = self.wizardSubmitUrl + self.formUuid + '/submit';
-                        }
-                    });
-                }
-            });
+            form.on('wizardPageSelected', (submission) => self.wizardStateChange(form, submission));
+            form.on('prevPage', (submission) => self.wizardStateChange(form, submission));
+            form.on('nextPage', (submission) => self.wizardStateChange(form, submission));
 
             // Set the Submission (data)
             // https://github.com/formio/formio.js/wiki/Form-Renderer#setting-the-submission
