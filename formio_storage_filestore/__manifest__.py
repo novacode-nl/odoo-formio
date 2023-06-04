6b789427276da92c1e722ca16c5861266ea96364
bobslee <bob@novacode.nl>
2023-06-04 10:12:29 +0200

    [DOC] formio_storage_filestore: name in manifest

--- formio_storage_filestore/__manifest__.py
+++ formio_storage_filestore/__manifest__.py
@@ -2,7 +2,7 @@
 # See LICENSE file for full licensing details.
 
 {
-    'name': 'Forms | Storage Filestore',
+    'name': 'Forms | Filestore Storage',
     'summary': 'Store uploads/files by URL in Odoo filestore (attachments)',
     'version': '1.1',
     'license': 'LGPL-3',
