7c2c12f61d7e5cb462c0bb4a9a370eb79c0bae8c
bobslee <bob@novacode.nl>
2023-07-11 09:15:52 +0200

    [IMP] formio_report_qweb: Use MarkupSafe to escape some components.
    Components:
    - content_component
    - html_component
    - textarea_component

--- formio_report_qweb/CHANGELOG.md
+++ formio_report_qweb/CHANGELOG.md
@@ -1,5 +1,12 @@
 # Changelog
 
+## 16.0.1.2
+
+Use MarkupSafe to escape components:
+- content_component
+- html_component
+- textarea_component
+
 ## 16.0.1.1
 
 Layout improvements for the report components, to comply with the (Odoo) Bootstrap upgrade from v4 to v5.
--- formio_report_qweb/__manifest__.py
+++ formio_report_qweb/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Forms | QWeb Reports',
     'summary': 'Generate (PDF) reports for every Form',
-    'version': '16.0.1.1',
+    'version': '16.0.1.2',
     'author': 'Nova Code',
     'website': 'https://www.novacode.nl',
     'live_test_url': 'https://demo16.novacode.nl',
