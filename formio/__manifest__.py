496abed2990fc0851e66fbf55a4c772992cc07ff
bobslee <bob@novacode.nl>
2023-09-12 12:25:09 +0200

    [IMP] formio: Rename formio.form field label submission_data
    Rename label of `formio.form` field `submission_data`, from `Data` to `Submission Data`.

--- formio/__manifest__.py
+++ formio/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Forms',
     'summary': 'Form Builder for backend, portal and website forms - to collect any information you need for your business.',
-    'version': '16.0.6.9',
+    'version': '16.0.6.10',
     'license': 'LGPL-3',
     'author': 'Nova Code',
     'website': 'https://www.novacode.nl',
