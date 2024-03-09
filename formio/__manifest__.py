0d0b4c3ab8cf280e7d8ef0bb5106ceda48c26aa8
bobslee <bob@novacode.nl>
2024-03-09 16:26:56 +0100

    [DOC] formio: manifest summary

--- formio/__manifest__.py
+++ formio/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Forms',
-    'summary': 'Form Builder for backend, portal and website forms - to collect any information you need for your business.',
+    'summary': 'Form Builder for internal, portal and website forms - to collect any information you need for your business.',
     'version': '15.0.16.17',
     'license': 'LGPL-3',
     'author': 'Nova Code',
