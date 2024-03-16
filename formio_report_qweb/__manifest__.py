0a042ad4fa8ba3054fed16301a9c5e24fe32d6d9
bobslee <bob@novacode.nl>
2024-03-16 12:01:58 +0100

    [DOC] Modules manifest: temporary change live_test_url

--- formio_report_qweb/__manifest__.py
+++ formio_report_qweb/__manifest__.py
@@ -7,7 +7,7 @@
     'version': '17.0.1.0',
     'author': 'Nova Code',
     'website': 'https://www.novaforms.app',
-    'live_test_url': 'https://demo17.novaforms.app',
+    'live_test_url': 'https://demo16.novacode.nl',
     'license': 'LGPL-3',
     'category': 'Forms/Forms',
     'depends': [
